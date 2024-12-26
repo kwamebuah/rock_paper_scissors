5c4de3ea4b095cf77318475e1aef7e8891a76842
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2024-12-26 08:09:32 +0000

    Create rock paper scissors game
    Create function to get user choice.
    Create function to generate random computer choice between 1 and 3.
    Create win conditions/results to be displayed to user.
    Limit user choice to only rock paper and scissors to avoid out of scope
    choices.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rock_paper_scissors
+
+First rock paper scissors in javascript.
+
+It will be run in console where the user will play against the computer.
---
+++ script.js
@@ -0,0 +1,89 @@
+// RPS
+// display choice of rock paper or scissors
+// ask user to input choice
+// store user choice
+// generate random choice of rock paper or scissors
+// store random choice
+// compare user choice (uc) with random choice (rc)
+// user win conditions:
+// 1. uc is rock and rc is scissors
+// 2. uc is paper and rc is rock
+// 3. uc is scissors and rc is paper
+// all else results in loss
+// if same, it is a tie
+// else compare win conditions
+// else loss
+
+function getUserChoice() {
+    console.log("Rock Paper Scissors");
+    let userChoice = Number(prompt("Choose:\n1 (rock)\n2 (paper)\n3 (scissors)"));
+    return userChoice;
+}
+
+function getCompChoice() {
+    let compChoice = Number(Math.floor(Math.random() * 3) + 1);
+    return compChoice;
+}
+
+function toDisplay(choice) {
+    let output;
+    // switch (choice)
+    // {
+    //     case 1:
+    //         output = 'rock';
+    //         break;
+    //     case 2:
+    //         output = 'paper';
+    //         break;
+    //     case 3:
+    //         output = 'scissors';
+    //         break;
+    // }
+
+
+    output = choice == 1 ? 'rock' :
+    choice == 2 ? 'paper': 'scissors';
+    return output;
+}
+
+let user = getUserChoice ();
+console.log(user);
+console.log(typeof(user));
+
+
+let computer = getCompChoice();
+console.log(computer);
+
+if (user !== 1 || user !== 2 || user !== 3) {
+    console.log('Please choose 1 2 or 3');
+}
+else {
+    let toShow = toDisplay(user);
+    console.log(`You chose ${toShow}`);
+    toShow = toDisplay(computer);
+    console.log(`Computer chose ${toShow}`);
+}
+
+let result;
+
+if (user === computer) {
+    result = "tied";
+}
+else if (user === 1 && computer === 3) {
+    result = "won";
+}
+else if (user === 2 && computer === 1) {
+    result = "won";
+}
+else if (user === 3 && computer === 2) {
+    result = "won";
+}
+else {
+    result = "lost";
+}
+
+console.log(result);
+
+
+// Score
+
